0befb519ce825c2e2ec1f65f9e439380d4ba7082
George Tang <georgethewebmaster@gmail.com>
2020-10-20 17:59:32 -0500

    first edit AttributeServlet

--- src/main/java/servlet/AttributeServlet.java
+++ src/main/java/servlet/AttributeServlet.java
@@ -20,6 +20,7 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
 
    String name   = request.getParameter("attrib_name");
    String value  = request.getParameter("attrib_value");
+   String color  = request.getParameter("attrib_color");
    String remove = request.getParameter("attrib_remove");
 
    if (remove != null && remove.equals("on"))
@@ -28,9 +29,9 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    }
    else
    {
-      if ((name != null && name.length() > 0) && (value != null && value.length() > 0))
+      if ((name != null && name.length() > 0) && (value != null && value.length() > 0) && (color != null && color.length() > 0))
       {
-         session.setAttribute(name, value);
+         session.setAttribute(name, value, color);
       }
 
    }
@@ -47,9 +48,9 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    out.println("");
 
    out.println("<body>");
-   out.println("<h1><center>Session attributes</center></h1>");
+   out.println("<h1><center>George Tang's Session attributes</center></h1>");
 
-   out.println("Enter name and value of an attribute");
+   out.println("Enter name, value, and color of an attribute");
 
 
    String url = response.encodeURL("attribute");
@@ -59,6 +60,9 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
 
    out.println(" Value: ");
    out.println(" <input type=\"text\" size=\"10\" name=\"attrib_value\">");
+
+   out.println(" Color: ");
+   out.println(" <input type=\"text\" size=\"10\" name=\"attrib_color\">");
 
    out.println(" <br><input type=\"checkbox\" name=\"attrib_remove\">Remove");
    out.println(" <input type=\"submit\" name=\"update\" value=\"Update\">");
@@ -71,11 +75,14 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    {
       String att_name  = (String) e.nextElement();
       String att_value = (String) session.getAttribute(att_name);
+      String att_color = (String) session.getAttribute(att_name);
 
       out.print  ("<br><b>Name:</b> ");
       out.println(att_name);
       out.print  ("<br><b>Value:</b> ");
       out.println(att_value);
+      out.print  ("<br><b>Color:</b> ");
+      out.println(att_color);
    } //end while
 
    out.println("</body>");
