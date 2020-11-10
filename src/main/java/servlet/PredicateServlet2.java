4d2104a9e0ce598262777ce63f2747e0c91c1244
George Tang <georgethewebmaster@gmail.com>
2020-11-09 22:34:51 -0600

    add necessary imports

--- src/main/java/servlet/PredicateServlet2.java
+++ src/main/java/servlet/PredicateServlet2.java
@@ -4,15 +4,22 @@
   @author Morgan Abreu, George Tang
   ********************************************************************* */
 
-import java.io.PrintWriter;
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+  import java.io.PrintWriter;
+  import java.io.IOException;
+  
+  import javax.servlet.ServletException;
+  import javax.servlet.ServletOutputStream;
+  import javax.servlet.annotation.WebServlet;
+  import javax.servlet.http.HttpServlet;
+  import javax.servlet.http.HttpServletRequest;
+  import javax.servlet.http.HttpServletResponse;
+  import javax.servlet.RequestDispatcher;
+  
+  import javax.script.ScriptEngine;
+  import javax.script.ScriptEngineManager;
+  import javax.script.ScriptException;
+  import java.util.List;
+  import java.util.ArrayList;
  
   // Adds servlet mapping annotation
   import javax.servlet.annotation.WebServlet;
